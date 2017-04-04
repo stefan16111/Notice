f2bfabbf8b05b08b52f1a114e79b2659ff4ed05f
stefan16111@wp.pl <stefan16111@wp.pl>
2017-04-04 12:03:06 +0200

    first commit

--- src/userBundle/Entity/Group.php
+++ src/userBundle/Entity/Group.php
@@ -1,8 +1,19 @@
 <?php
+namespace userBundle\Entity;
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+use FOS\UserBundle\Model\Group as BaseGroup;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_group")
  */
-
+class Group extends BaseGroup
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+     protected $id;
+}
